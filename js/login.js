861747796e542891f892a3f9519649426b3cb891
Carolina Sánchez <carolinasanchez.arq@gmail.com>
2020-08-11 18:06:36 -0300

    Entrega 1 v.2
    Correccion de Index

---
+++ js/login.js
@@ -0,0 +1,17 @@
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+
+function MyFunction() {
+    var user = $("#loginuser").val();
+    var pass = $("#loginpass").val();
+
+    if (user == "") {
+        $("#alertuser").text("Debe ingresar nombre de usuario");
+    } else if (pass.length < 8) {
+        $("#alertpass").text("Debe ingresar contraseña mayor a 8 caracteres");
+    } else {
+        sessionStorage.setItem('user', user);
+        window.location.href = 'index.html';
+    };
+};
