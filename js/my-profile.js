f491d06c242f1e64b3cdf04d5bfdd586b95e2a47
Carolina Sánchez <carolinasanchez.arq@gmail.com>
2020-11-04 14:19:23 -0300

    E07.05
    JSON usuario

--- js/my-profile.js
+++ js/my-profile.js
@@ -2,59 +2,43 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e) {
-    document.getElementById("u-nombre").value = localStorage.getItem('nuevonombre');
-    document.getElementById("u-apellido").value = localStorage.getItem('nuevoapellido');
-    document.getElementById("u-email").value = localStorage.getItem('nuevoemail');
-    document.getElementById("u-telefono").value = localStorage.getItem('nuevotelefono');
-    document.getElementById("u-edad").value = localStorage.getItem('nuevoedad');
+    //convierte JSON en objeto JS para leer datos almacenados en localStorage;
+    var infoUsuario = JSON.parse(localStorage.getItem("infoUsuario"));
+    document.getElementById("u-nombre").value = infoUsuario.nombre;
+    document.getElementById("u-apellido").value = infoUsuario.apellido;
+    document.getElementById("u-email").value = infoUsuario.email;
+    document.getElementById("u-telefono").value = infoUsuario.telefono;
+    document.getElementById("u-edad").value = infoUsuario.edad;
     document.getElementById('base64').src = "data:image/jpeg;base64," + localStorage.getItem('nuevafoto');
 });
 
-var infoUsuario = {
-    "nombre": "",
-    "apellido": "",
-    "email": "",
-    "telefono": "",
-    "edad": "",
-};
 
 //Funcion que almacena datos de usuario con modificaciones en localStorage y crea JSON
 function guardarDatos() {
     //nombre
     var usuarionombre = $("#u-nombre").val();
-    localStorage.setItem('nuevonombre', usuarionombre);
     document.getElementById("u-nombre").value = usuarionombre;
     //apellido
     var usuarioapellido = $("#u-apellido").val();
-    localStorage.setItem('nuevoapellido', usuarioapellido);
     document.getElementById("u-apellido").value = usuarioapellido;
     //email
     var usuarioemail = $("#u-email").val();
-    localStorage.setItem('nuevoemail', usuarioemail);
     document.getElementById("u-email").value = usuarioemail;
     //telefono
     var usuariotelefono = $("#u-telefono").val();
-    localStorage.setItem('nuevotelefono', usuariotelefono);
     document.getElementById("u-telefono").value = usuariotelefono;
     //edad
     var usuarioedad = $("#u-edad").val();
-    localStorage.setItem('nuevoedad', usuarioedad);
     document.getElementById("u-edad").value = usuarioedad;
-    infoUsuario = {
+    var infoUsuario = {
         "nombre": usuarionombre,
         "apellido": usuarioapellido,
         "email": usuarioemail,
         "telefono": usuariotelefono,
         "edad": usuarioedad,
     };
-};
-
-
-//Funcion que valida datos obligatorios
-function validarDatos() {
-    if (usuarionombre == "" && usuariomail == "") {
-        document.getElementById('error-datos').innerHTML = "Debes completar los datos obligatorios";
-    };
+    //convierte objeto JS en JSON para que los datos sean almacenados en localStorage;
+    localStorage.setItem("infoUsuario", JSON.stringify(infoUsuario));
 };
 
 
